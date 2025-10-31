478878127f1f51cd8fa237918c037d38a98599d2
THNisme <nghiath.ce191115@gmail.com>
2025-10-31 16:43:29 +0700

    setup folder and env

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import './Q2.1.js';
-import './Q2.2.js';
-import './CQ2.4.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
