6e88bd94938b7f715bb777aeda981aefc9a54242
KietTT <kiettt.ce190871@gmail.com>
2025-11-03 17:04:07 +0700

    add file MessageDetailPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes , Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Các bạn import component vào đây, như ví dụ của navbar và footer bên dưới
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ContactPage from "./pages/ContactPage";
 import MessagesPage from "./pages/MessagesPage";
+import MessageDetailPage from "./pages/MessageDetailPage";
 
 // Các bạn import page vào đây, như ví dụ bên dưới
 // import Home from "./pages/Home";
@@ -25,9 +26,9 @@ function App() {
             {/* Các bạn setup Route như ví dụ bên dưới nhaa */}
             {/* <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} /> */}
-                      <Route path="/contact" element={<ContactPage />} />
-                      <Route path="/messages" element={<MessagesPage />} />
-        
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/messages" element={<MessagesPage />} />
+            <Route path="/messages/:id" element={<MessageDetailPage />} />
           </Routes>
         </main>
 
