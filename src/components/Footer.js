5c6f0dafddce45457729ec18f80be2d48f11d22f
THNisme <nghiath.ce191115@gmail.com>
2025-11-10 00:49:18 +0700

    fix grid homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -42,7 +42,7 @@ const Footer = () => {
               <Col>
                 <ul className='footer-button-list'>
                   <li className='footer-button-item'>
-                    <Button as={Link} to={"/admin-path"} variant='outline-light' className='button-shape mb-3'>Tạo sự kiện</Button>
+                    <Button as={Link} to={"/adminselection"} variant='outline-light' className='button-shape mb-3'>Tạo sự kiện</Button>
                   </li>
                   <li className='footer-button-item'>
                     <Button as={Link} to={"/contact"} variant='outline-light' className='button-shape mb-3'>
