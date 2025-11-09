07bf3598b055ebf0f8d6a42152caebc58d638978
KietLTCE191226 <Kietltce191226@gmail.com>
2025-11-09 10:23:55 +0700

    này là coppy sang file mới luôn nên paste từ file cũ qua thôi

---
+++ src/assets/imageList.js
@@ -0,0 +1,21 @@
+const imageList = [
+  {
+    id: 1,
+    title: "Ảnh ngẫu nhiên 1",
+    url: "https://picsum.photos/1280/720?random=1"
+  },
+  {
+    id: 2,
+    title: "Ảnh ngẫu nhiên 2",
+    url: "https://picsum.photos/1280/720?random=2"
+  },
+  {
+    id: 3,
+    title: "Ảnh Quơ Tour - MTTN",
+    url: "assets/images/event-img.png"
+  },
+
+
+];
+
+export default imageList;
